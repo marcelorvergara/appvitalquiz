fb994b1c701f19016e9282260ae71a936b735b6e
Marcelo Vergara <marcelorv@gmail.com>
2023-11-15 14:48:40 -0300

    Logout func

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AuthService } from './components/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +13,25 @@ import { HomeComponent } from './pages/home/home.component';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'vital-quiz-2';
+  title = 'vital-quiz';
+  isAuthenticated = false;
+  private userSub: Subscription | undefined;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !!user;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.userSub) {
+      this.userSub.unsubscribe();
+    }
+  }
+
+  logOutUser() {
+    this.authService.logOut();
+  }
 }
