93732cd8da5537527792825b248fd87833eb88f2
Marcelo Vergara <marcelorv@gmail.com>
2023-11-26 10:01:31 -0300

    Chart working!

--- src/app/components/patients/patient-results/patient-results.component.ts
+++ src/app/components/patients/patient-results/patient-results.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataStorageService } from '../../../shared/data-storage.service';
-import { MapValue, DateFields } from '../../../shared/models/patient.model';
+import { DateFields } from '../../../shared/models/patient.model';
 import { EChartsOption } from 'echarts';
 import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
 
@@ -19,11 +19,7 @@ type MapOfDateAndResults = {
 })
 export class PatientResultsComponent implements OnInit {
   patientResultsPerDate: DateFields | null = null;
-  initOpts = {
-    renderer: 'svg',
-    width: 360,
-    height: 300,
-  };
+
   options: EChartsOption = {};
 
   constructor(private dataStorageService: DataStorageService) {}
@@ -32,24 +28,35 @@ export class PatientResultsComponent implements OnInit {
     // Get and prepare the test results of the patient
     this.patientResultsPerDate =
       this.dataStorageService.getPatientResultData() || null;
-    const mapOfResults: MapOfDateAndResults = this.sumResultsPerTest(
+    const { sumPsychic, sumSomatic, totSum } = this.sumResultsPerTest(
       this.patientResultsPerDate
     );
-    console.log(mapOfResults);
+
     // Graph
     const xAxisData = [];
-    const data1 = [];
+    const totDataPerDay = [];
+    const sumPsychicPerDay = [];
+    const sumSomaticPerDay = [];
     // const data2 = [];
 
     // Sort the days of the report
-    const mapOfResultsSorted = this.sortObjectByDate(mapOfResults);
+    const mapOfResultsSorted = this.sortObjectByDate(totSum);
     for (const key in mapOfResultsSorted) {
       xAxisData.push(key.replace('_', '').replaceAll('_', '/'));
-      data1.push(mapOfResults[key]);
+      totDataPerDay.push(totSum[key]);
     }
+    const mapOfPsychicResultsSorted = this.sortObjectByDate(sumPsychic);
+    for (const key in mapOfPsychicResultsSorted) {
+      sumPsychicPerDay.push(sumPsychic[key]);
+    }
+    const mapOfSomaticResultsSorted = this.sortObjectByDate(sumSomatic);
+    for (const key in mapOfSomaticResultsSorted) {
+      sumSomaticPerDay.push(sumSomatic[key]);
+    }
+
     this.options = {
       legend: {
-        data: ['Resultado'],
+        data: ['Totais', 'Psíquica', 'Somática'],
         align: 'left',
       },
       tooltip: {},
@@ -69,9 +76,21 @@ export class PatientResultsComponent implements OnInit {
       },
       series: [
         {
-          name: 'Resultado',
+          name: 'Totais',
           type: 'line',
-          data: data1,
+          data: totDataPerDay,
+          animationDelay: (idx) => idx * 10,
+        },
+        {
+          name: 'Psíquica',
+          type: 'line',
+          data: sumPsychicPerDay,
+          animationDelay: (idx) => idx * 10,
+        },
+        {
+          name: 'Somática',
+          type: 'line',
+          data: sumSomaticPerDay,
           animationDelay: (idx) => idx * 10,
         },
       ],
@@ -81,13 +100,35 @@ export class PatientResultsComponent implements OnInit {
   }
 
   sumResultsPerTest(results: DateFields | null) {
-    let sums: MapOfDateAndResults = {};
+    let sumPsychic: MapOfDateAndResults = {};
+    let sumSomatic: MapOfDateAndResults = {};
+    let totSum: MapOfDateAndResults = {};
     for (const key in results) {
       if (
         results[key].hasOwnProperty('arrayValue') &&
         Array.isArray(results[key].arrayValue.values)
       ) {
-        sums[key] = results[key].arrayValue.values.reduce((sum, item) => {
+        sumPsychic[key] = results[key].arrayValue.values
+          .slice(0, 6)
+          .reduce((sum, item) => {
+            return (
+              sum +
+              (item.hasOwnProperty('integerValue')
+                ? parseInt(item.integerValue, 10)
+                : 0)
+            );
+          }, 0);
+        sumSomatic[key] = results[key].arrayValue.values
+          .slice(-6)
+          .reduce((sum, item) => {
+            return (
+              sum +
+              (item.hasOwnProperty('integerValue')
+                ? parseInt(item.integerValue, 10)
+                : 0)
+            );
+          }, 0);
+        totSum[key] = results[key].arrayValue.values.reduce((sum, item) => {
           return (
             sum +
             (item.hasOwnProperty('integerValue')
@@ -97,7 +138,7 @@ export class PatientResultsComponent implements OnInit {
         }, 0);
       }
     }
-    return sums;
+    return { sumPsychic, sumSomatic, totSum };
   }
 
   sortObjectByDate(obj: { [key: string]: number }): { [key: string]: number } {
